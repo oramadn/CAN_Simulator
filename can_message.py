51d6f485dae4361663b81e875c1d8def006b94c6
oramadn <omar.ramadanpersonal@gmail.com>
2024-04-29 20:48:00 +0300

    Removed unwanted files Added can_meassage and variable_frame classes

---
+++ can_message.py
@@ -0,0 +1,11 @@
+import time
+import random
+
+
+class CANmsg:
+    def __init__(self, id=None, data=None):
+        self.id = id
+        self.data = data
+
+
+
--- main.py
+++ main.py
@@ -1,25 +1,60 @@
 import sys
-import src
+import can_message
 import random
+from variable_frame import VariableFrame
+from can_message import CANmsg
 import primaryWindow
 from PySide6.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QInputDialog
 from PySide6.QtCore import Slot, QTimer
 
 
-class PrimaryWindowClass(QMainWindow, primaryWindow.Ui_primaryWindow):
-    data = 0
+class PrimaryWindow(QMainWindow, primaryWindow.Ui_primaryWindow):
+    frames = 0
     variable_frames = []
+    variable_frames_copy = 0
+    chosen_variable_frames = []
     static_frames = []
-    variable_bytes_idx = []
-    throttle_bytes = []
-    brake_bytes = []
-    steering_bytes = []
+    throttle_idx = 0
+    brake_idx = 0
+    steering_idx = []
+    variable_frames_idx = []
+    generated_hex_numbers = []
 
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.resize(950, 1000)
-        self.startButton.clicked.connect(self.generate_data)
+        self.startButton.clicked.connect(self.start_simulation)
+        self.variable_frames_timer = QTimer(self)
+        self.variable_frames_timer.timeout.connect(self.simulate_variable_frames)
+
+    def simulate_variable_frames(self):
+        for frame in self.chosen_variable_frames:
+            self.variable_frames_copy[frame.idx].data = random.choice(frame.random_hex_numbers)
+            self.clear_table()
+            self.generate_table(self.variable_frames_copy + self.static_frames)
+
+    def generate_fixed_hex(self, num_of_random_hex):
+        # Calculate the number of bytes the hex number should have
+        num_bytes = 8  # Each byte is represented by two hex characters
+        # random.seed(42)
+        for frame in self.chosen_variable_frames:
+            for _ in range(random.randint(2, num_of_random_hex)):
+                # Generate a random integer with the right number of bits (8 bits per byte)
+                random_int = random.getrandbits(num_bytes * 8)
+                # Convert integer to a hexadecimal string, and format to ensure correct length
+                random_hex = format(random_int, '0{}x'.format(num_bytes * 2))
+                # Append the formatted hex string to the list
+                frame.random_hex_numbers.append(random_hex)
+
+    def generate_variable_frames(self):
+        frames = list(range(len(self.variable_frames)))
+        random.shuffle(frames)
+        self.throttle_idx, self.brake_idx, self.steering_idx, self.variable_frames_idx = frames[0], frames[1], frames[2:4], frames[4:]
+
+    def clear_table(self):
+        self.mainTable.clearContents()
+        self.mainTable.setRowCount(0)
 
     def add_row(self, row_data):
         row_position = self.mainTable.rowCount()
@@ -28,9 +63,41 @@ class PrimaryWindowClass(QMainWindow, primaryWindow.Ui_primaryWindow):
             item = QTableWidgetItem(str(data))
             self.mainTable.setItem(row_position, column, item)
 
-    def clear_table(self):
-        self.mainTable.clearContents()
-        self.mainTable.setRowCount(0)
+    def generate_table(self, frames):
+        data_row = []
+        # Format the data such that it can fit into 9 columns (ID , 0-7) where each column has 1 byte
+        for frame in frames:
+            data_row.append("0x" + frame.id)
+            for i in range(0, len(frame.data), 2):
+                data_row.append("0x" + frame.data[i:i + 2])
+            data_row_copy = data_row.copy()
+            self.add_row(data_row_copy)
+            data_row.clear()
+
+    def split_frames(self, class_list, ratio=0.6):
+        random.shuffle(class_list)
+
+        split_index = int(len(class_list) * ratio)
+
+        variable_frames = class_list[:split_index]
+        static_frames = class_list[split_index:]
+
+        return variable_frames, static_frames
+
+    def generate_frames(self, frame_count):
+        # Generate random IDs and DATA
+        data = []
+        for i in range(frame_count):
+            random_id = hex(random.randint(0, 2 ** 11 - 1))[2:].rjust(5, '0')
+            random_data = hex(random.randint(0, 2 ** 64 - 1))[2:].rjust(16, '0')
+
+            data.append(CANmsg(random_id, random_data))
+
+        data = sorted(data, key=lambda x: x.id)
+        return data
+
+        # data = generateFrames(frame_count) #list of CANmsg objects
+        # generateTable(data)
 
     def open_input_dialog(self):
         while True:
@@ -46,52 +113,43 @@ class PrimaryWindowClass(QMainWindow, primaryWindow.Ui_primaryWindow):
             else:
                 return 0
 
-    # def simulateVariableBytes(self):
-    #     for frame in self.variableFrames:
-
-    @Slot()
-    def generate_data(self):
-        print('\nGenerate start\n')
-        frame_count = self.open_input_dialog()
-        self.data = src.generate_frames(
-            frame_count)  # REMOVE THE ADDED "0x" IN generateFrames AND FIX THE CODE AS SUCH!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-        if frame_count != 0:  # If the user hits cancel when prompted to enter number of needed frames
-            data_row = []
-            # Format the data such that it can fit into 9 columns (ID , 0-7) where each column has 1 byte
-            for frame in self.data:
-                data_row.append(frame.id)
-                for i in range(2, len(frame.frames), 2):
-                    data_row.append("0x" + frame.frames[i:i + 2])
-                data_row_copy = data_row.copy()
-                self.add_row(data_row_copy)
-                data_row.clear()
-            self.startButton.hide()  # Grey out the start button
-            print('\nGenerate finish\n')
-
-            self.start_simulation()
-
     @Slot()
     def start_simulation(self):
         print("Simulation start")
-        # Split data in variable and static frames with a ratio of 0.6 to 0.4
-        self.variable_frames, self.static_frames = src.split_frames(self.data)
-        print(self.variable_frames[0].frames)
-        # Generate indexes at random for variable bytes
-        self.variable_bytes_idx, self.throttle_bytes, self.brake_bytes, self.steering_bytes = src.generate_variable_bytes_idx(
-            self.variable_frames)
+        frame_count = self.open_input_dialog()
 
-        # Running
-        # self.timer = QTimer(self)
-        # self.timer.timeout.connect(self.simulateVariableBytes)
-        # self.timer.start(1000)  # Call every 1000 milliseconds (1 second)
+        # Generate the frames
+        self.frames = self.generate_frames(
+            frame_count)
 
-        print("Simulation finish")
+        if frame_count != 0:  # If the user hits cancel when prompted to enter number of needed frames
+            # Split data in variable and static frames with a ratio of 0.6 to 0.4
+            self.variable_frames, self.static_frames = self.split_frames(self.frames)
+            self.variable_frames_copy = self.variable_frames.copy()
+
+            self.generate_table(self.frames)
+            self.startButton.hide()  # Grey out the start button
+
+            # Generate indices for throttle, break, steering and the rest
+            self.generate_variable_frames()
+
+            # Create VariableFrame objects using the variable frame indices
+            for i in self.variable_frames_idx:
+                self.chosen_variable_frames.append(VariableFrame(self.variable_frames[i].id,
+                                                                 self.variable_frames[i].data, i))
+
+            # Generate random frames for the variable frames
+            self.generate_fixed_hex(5)
+
+            # Start the simulation
+            self.variable_frames_timer.start(1000)  # Call every 1000 milliseconds (1 second)
+
+            print("Program ended")
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = PrimaryWindowClass()
+    window = PrimaryWindow()
 
     window.show()
     sys.exit(app.exec())
