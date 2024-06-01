51b06d8ff0a414d062a6ce905bc61b90a5147e9e
oramadn <omar.ramadanpersonal@gmail.com>
2024-06-01 12:44:28 +0300

    Updated sniffer to inject

--- arduino codes/Sniffer/Sniffer.ino
+++ arduino codes/Sniffer/Sniffer.ino
@@ -1,20 +1,36 @@
 #include <mcp2515.h>
 #include <SPI.h>
 
-MCP2515 mcp2515(10);  // Set your CS pin here
+MCP2515 mcp2515(10); // Set your CS pin here
+ struct can_frame canMsg;
+ struct can_frame canMsg1;
+
 void setup() {
-  Serial.begin(500000);  // Initialize serial communication at 500000 baud rate
-  while (Serial.available() > 0) {
-    Serial.read(); // Clear the buffer
-  }
+  Serial.begin(500000); // Initialize serial communication at 500000 baud rate
   SPI.begin();
   mcp2515.reset();
-  mcp2515.setBitrate(CAN_1000KBPS, MCP_8MHZ);  // Set bitrate to 500kbps
+  mcp2515.setBitrate(CAN_1000KBPS, MCP_8MHZ); // Set bitrate to 1000kbps
   mcp2515.setNormalMode();
   Serial.println("CAN Sniffer ready");
 }
 
 void loop() {
+  // Check for serial data
+  if (Serial.available() > 0) {
+    String serialData = Serial.readStringUntil('\n');
+    if (serialData.length() == 6) { // 3 characters for ID and 3 characters for value
+      String idString = serialData.substring(0, 3);
+      String valueString = serialData.substring(3, 6);
+    
+      int canID = strtol(idString.c_str(), NULL, 16);
+      int value = strtol(valueString.c_str(), NULL, 10);
+
+      canMsg1.can_id = canID;
+      canMsg1.can_dlc = 1; // Data length code, assuming we're only sending 1 byte
+      canMsg1.data[0] = value;
+  }}
+
+  // Sniffer functionality remains unchanged
   struct can_frame canMsg;
   if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
     // Successfully received a CAN message
@@ -26,6 +42,8 @@ void loop() {
     sprintf(output + len, "\"}");
     Serial.println(output);
   }
-  
-}
 
+  if (canMsg1.data[0] != 0){
+    mcp2515.sendMessage(&canMsg1);
+  }
+}
